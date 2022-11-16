8c3d1b7e520c4e728f1c1f6d4b7e989fd1e86681
PROENCA Sandrine (SNCF VOYAGEURS / ESV TGV Sud Ouest / BCC TOULOUSE) <7410499r@commun.ad.sncf.fr>
2022-11-16 14:08:29 +0100

    randomShifumi

--- script.js
+++ script.js
@@ -11,9 +11,22 @@ mi.addEventListener('click', buttonMi);
 
 // Pour le reste, a vous de jouer
 
+/*Sandrine */
+/* cration des variables, des constantes et tableaux des idéogrammes*/
+const bouton = document.getElementById("random");
+const texte = document.getElementById("resultat");
+const iaIdeogrammes = ['Shi','Fu','Mi'];
+const nbIdeogrammes = iaIdeogrammes.length;
+
+/* déclanchement de la fonction randomize sur le clic du bouton*/
+bouton.addEventListener('click',randomize);
+
+/* fonction choisissant un idéogramme au hasard dans le tableau */
 function randomShifumi(){
-    
+    texte.innerHTML = iaIdeogrammes[Math.floor(Math.random()*nbIdeogrammes)];
+
 }
+
 
 function buttonShi(){
 
