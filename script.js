93842bd488f6f0c77adf1fd3946c5679e96bf491
SANFILIPPO Jean-Francois (SNCF VOYAGEURS / ET TGV ATLANTIQUE / Territoire Occitanie) <7407117p@commun.ad.sncf.fr>
2022-11-16 12:04:08 +0100

    v0.1 les 3 fonctions Shi Fu Mi

--- script.js
+++ script.js
@@ -5,8 +5,20 @@ const player = document.getElementById('player');
 const ia = document.getElementById('ia');
 const command = document.getElementById('command');
 
-shi.addEventListener('click', () => console.log('shi'));
-fu.addEventListener('click', () => console.log('fu'));
-mi.addEventListener('click', () => console.log('mi'));
+shi.addEventListener('click', buttonShi);
+fu.addEventListener('click', buttonFu);
+mi.addEventListener('click', buttonMi);
 
 // Pour le reste, a vous de jouer
+
+function buttonShi(){
+
+}
+
+function buttonFu(){
+
+}
+
+function buttonMi(){
+    
+}
