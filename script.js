677cb3998a7be5b6c8e1c6f9ec066bade7f9c49b
SANFILIPPO Jean-Francois (SNCF VOYAGEURS / ET TGV ATLANTIQUE / Territoire Occitanie) <7407117p@commun.ad.sncf.fr>
2022-11-16 16:13:10 +0100

    v0.36 correction affichage img

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const player = document.getElementById('player');
 const ia = document.getElementById('ia');
 const command = document.getElementById('command');
 let choixIA;
-let ScoreIA=0;
-let ScorePlayer=0;
+let ScoreIA;
+let ScorePlayer;
 
 shi.addEventListener('click', buttonShi);
 fu.addEventListener('click', buttonFu);
@@ -23,18 +23,6 @@ const nbIdeogrammes = iaIdeogrammes.length;
 /* fonction choisissant un idéogramme au hasard dans le tableau */
 function randomShifumi(){
     return iaIdeogrammes[Math.floor(Math.random()*nbIdeogrammes)];
-   
-
-}
-
-function imageIa (){
-    if(randomShifumi==shi){
-        return ia.setAttribute("src","/img/shi.png")
-    }else if(randomShifumi==fu){
-        return ia.setAttribute("src","/img/fu.png")
-    }else(randomShifumi==mi){
-        return ia.setAttribute("src","/img/mi.png")
-    }
 }
 
 function buttonShi(){
@@ -67,17 +55,11 @@ function buttonFu(){
 
 function buttonMi(){
     choixIA = randomShifumi();
-<<<<<<< HEAD
-    player.setAttribute("src","/img/mi.png")
-    console.log(`choix du joueur Mi, choix du IA ${choixIA}`);
-    if (choixIA == "Mi"){
-=======
     player.src = "/img/mi.png";
     ia.src = `/img/${choixIA}.png`;
     if (choixIA == "mi"){
->>>>>>> 734b04cdb921123268edc99ceded3fcb871bc5c8
         return"egalité";
-    }else if (choixIA == "Fu"){
+    }else if (choixIA == "fu"){
         return "perdu";
     }else {
         return "gagné";
